13b730c5fac4c99ac7cc6e94bb1adaa0dadaefda
Luis Hernández <ldhdv95@gmail.com>
2018-11-19 18:53:58 -0500

    Create connection to my api between the server configuration and the routes file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000);
+
 //Middlewares
+app.use(express.json()); //Para que el servidor compruebe si el dato es en formato json.
+app.use('/api/tasks', require('./routes/task'));
 
 //Routes
 
