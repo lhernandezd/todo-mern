fe45fc431de9ffc22adfc63995466b9e08484b91
Luis Hernández <ldhdv95@gmail.com>
2018-11-20 18:09:06 -0500

    Add react modules

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <section class="App">
+        <h1>Hello World!!</h1>
+      </section>
+    )
+  };
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
-alert('Hola mundo')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
