94b1993fc46a1113c82b49ecca40856dd8e409ad
Luis Hernández <ldhdv95@gmail.com>
2018-11-28 19:21:18 -0500

    Changing stateless components: class to function

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -2,40 +2,41 @@ import React from 'react';
 import '../styles/Menu.css';
 import { Button, Icon, Modal, Header, Form } from 'semantic-ui-react';
 
-export default class Menu extends React.Component {
-  render() {
-    const button = (
-      <Button basic color='black' size='large' animated='fade' onClick={this.props.switchModal}>
-        <Button.Content visible>Add task</Button.Content>
-        <Button.Content hidden> <Icon name='add' /> </Button.Content>
-      </Button>
-    )
-    return (
-      <div className='menu'>
-        <div className='menu__item'>
-          <Modal trigger={button} open={this.props.modalOpen} closeIcon onClose={this.props.switchModal}>
-            <Header icon='plus' content='Add Task' />
-            <Modal.Content>
-              <Form size='large' className='modalForm' onSubmit={this.props.handleAdd}>
-                <Form.Field>
-                  <label>Title</label>
-                  <input name='modalTitle' onChange={this.props.handleChange} />
-                </Form.Field>
-                <Form.Field>
-                  <label>Description</label>
-                  <input name='modalDescription' onChange={this.props.handleChange} />
-                </Form.Field>
-                <Button type='submit' basic fluid color='green'>Create</Button>
-              </Form>
-            </Modal.Content>
-          </Modal>
-        </div>
-        <div className='menu__info'>
-          <span className='info__icon'><Icon name='info circle' />:</span>
-          <span className='info__text--completed'>Completed</span>
-          <span className='info__text--incompleted'>Incompleted</span>
-        </div>
+const Menu = (props) => {
+  const button = (
+    <Button basic color='black' size='large' animated='fade' onClick={props.switchModal}>
+      <Button.Content visible>Add task</Button.Content>
+      <Button.Content hidden> <Icon name='add' /> </Button.Content>
+    </Button>
+  );
+
+  return (
+    <div className='menu'>
+      <div className='menu__item'>
+        <Modal trigger={button} open={props.modalOpen} closeIcon onClose={props.switchModal}>
+          <Header icon='plus' content='Add Task' />
+          <Modal.Content>
+            <Form size='large' className='modalForm' onSubmit={props.handleAdd}>
+              <Form.Field>
+                <label>Title</label>
+                <input name='modalTitle' onChange={props.handleChange} />
+              </Form.Field>
+              <Form.Field>
+                <label>Description</label>
+                <input name='modalDescription' onChange={props.handleChange} />
+              </Form.Field>
+              <Button type='submit' basic fluid color='green'>Create</Button>
+            </Form>
+          </Modal.Content>
+        </Modal>
       </div>
-    )
-  }
+      <div className='menu__info'>
+        <span className='info__icon'><Icon name='info circle' />:</span>
+        <span className='info__text--completed'>Completed</span>
+        <span className='info__text--incompleted'>Incompleted</span>
+      </div>
+    </div>
+  )
 }
+
+export default Menu;
--- src/app/components/TodoForm.js
+++ src/app/components/TodoForm.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Form, Card, Button } from 'semantic-ui-react';
 
-export default class TodoForm extends React.Component {
-  render() {
-    return (
-      <section className="todoForm" style={{ margin: '14px' }}>
-        <Card>
-          <Card.Content>
-            <Form onSubmit={this.props.handleUpdate} size='large'>
-              <Form.Field>
-                <label>Title</label>
-                <input onChange={this.props.handleChange} placeholder={this.props.title} name='title' />
-              </Form.Field>
-              <Form.Field>
-                <label>Description</label>
-                <input onChange={this.props.handleChange} placeholder={this.props.description} name='description' />
-              </Form.Field>
-              <div className='ui two buttons'>
-                <Button type='submit' basic fluid color='blue'>Update</Button>
-                <Button basic fluid color='red'>Cancel</Button>
-              </div>
-            </Form>
-          </Card.Content>
-        </Card>
-      </section>
-    )
-  }
+const TodoForm = (props) => {
+  return (
+    <section className="todoForm" style={{ margin: '14px' }}>
+      <Card>
+        <Card.Content>
+          <Form onSubmit={props.handleUpdate} size='large'>
+            <Form.Field>
+              <label>Title</label>
+              <input onChange={props.handleChange} placeholder={props.title} name='title' />
+            </Form.Field>
+            <Form.Field>
+              <label>Description</label>
+              <input onChange={props.handleChange} placeholder={props.description} name='description' />
+            </Form.Field>
+            <div className='ui two buttons'>
+              <Button type='submit' basic fluid color='blue'>Update</Button>
+              <Button basic fluid color='red'>Cancel</Button>
+            </div>
+          </Form>
+        </Card.Content>
+      </Card>
+    </section>
+  )
 }
+
+export default TodoForm;
